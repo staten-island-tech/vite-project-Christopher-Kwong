c3976ff92292e0194acaafa14acdfadea1329cbc
Christopher Kwong <ckwong6599@gmail.com>
2022-12-18 15:12:40 -0500

    12/18/2022

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,43 +4,12 @@ import "aos/dist/aos.css";
 import { DOM } from "./dom";
 import { Colors } from "./color";
 import { ProductArray } from "./array";
-import { CreatingCard } from "./card";
-import { Button } from "./button";
-
-AOS.init();
-
-ProductArray.forEach((card) => CreatingCard(card));
-
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AN", "Antartica"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AF", "Africa"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AS", "Asia"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AU", "Australia"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("EU", "Europe"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("NA", "North America"));
-Button.Insert(DOM.ButtonBox, Button.ButtonCreate("SA", "South America"));
-
-function FilterNA() {
-  let filtered = ProductArray.filter((element) =>
-    element.Continent.includes("NA")
-  );
-  DOM.CardBox.innerHTML = "";
-  filtered.forEach((CardInfo) => CreatingCard(CardInfo));
-}
-
-FilterNA();
-/* 
-import "../css/style.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { DOM } from "./dom";
-import { Colors } from "./color";
-import { ProductArray } from "./array";
 import { Card } from "./card";
 import { Button } from "./button";
 
 AOS.init();
 
-Card.Creation(ProductArray);
+Card.CreatingCard(ProductArray);
 
 Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AN", "Antartica"));
 Button.Insert(DOM.ButtonBox, Button.ButtonCreate("AF", "Africa"));
@@ -55,7 +24,11 @@ function FilterNA() {
     element.Continent.includes("NA")
   );
   DOM.CardBox.innerHTML = "";
-  filtered.forEach((element) => NewCreating(element));
+  Card.CreatingCard(filtered);
 }
 
-FilterNA(); */
+document
+  .getElementById("filterbutton-NA")
+  .addEventListener("click", function () {
+    FilterNA();
+  });
